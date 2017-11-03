e877a157b9a37bdb9ec81532e843d4060f214efd
Mousa Hakim <yasirhakimjan@gmail.com>
2017-11-03 09:50:00 +0430

    Add student directory module
    Add student directory module basics functions

--- idealschool/settings.py
+++ idealschool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6w-0^k9^q_0nsxdin+udintacsrs0d3o+5u!w5j#+!+bii)v3_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.69.11.82', '172.16.68.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/'
+MEDIA_ROOT = '/home/yasir/idealschool/'
